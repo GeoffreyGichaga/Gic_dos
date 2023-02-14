5df49937250e6566e1c74b8b113da99e6dc6e608
Jeff <geofreygichaga@gmail.com>
2023-02-14 18:36:37 +0300

    add a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
